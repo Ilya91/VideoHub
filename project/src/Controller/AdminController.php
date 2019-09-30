9f7c1893ca22421e7e9a023e08d9b1d3895621f6
Mironchikov Ilya <i.mironchikov@gmail.com>
2019-09-30 10:33:17 +0300

    migrations and fixtures

--- project/src/Controller/AdminController.php
+++ project/src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,5 +20,19 @@ class AdminController extends AbstractController
     public function index()
     {
         return $this->render('admin/my_profile.html.twig');
+    }
+
+    /**
+     * @Route("/categories", name="admin_categories")
+     * @Template("/admin")
+     */
+    public function categories()
+    {
+        $repository = $this->getDoctrine()->getRepository(Category::class);
+        $categories = $repository->find(1);
+        //dump($categories);
+        return $this->render('admin/categories.html.twig', [
+            //'categories' => $categories
+        ]);
     }
 }
