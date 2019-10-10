15a21117f0f60c75216e3813c4308ea4bfdf5a51
Ilya Mironchikov <i.mironchikov@gmail.com>
2019-10-10 08:55:33 +0300

    registration form

--- project/src/Controller/FrontController.php
+++ project/src/Controller/FrontController.php
@@ -3,13 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\User;
 use App\Entity\Video;
+use App\Form\UserType;
 use App\Utils\CategoryTreeFrontPage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class FrontController extends AbstractController
 {
@@ -86,10 +90,33 @@ class FrontController extends AbstractController
 
     /**
      * @Route("/register", name="register")
+     * @param Request $request
+     * @return Response
      */
-    public function register()
+    public function register(UserPasswordEncoderInterface $password_encoder, Request $request)
     {
-        return $this->render('front/register.html.twig');
+        $user = new User;
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $user->setName($request->request->get('user')['name']);
+            $user->setLastName($request->request->get('user')['last_name']);
+            $user->setEmail($request->request->get('user')['email']);
+            $password = $password_encoder->encodePassword($user, $request->request->get('user')['password']['first']);
+            $user->setPassword($password);
+            $user->setRoles(['ROLE_USER']);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->loginUserAutomatically($user, $password);
+
+            return $this->redirectToRoute('admin_main_page');
+        }
+        return $this->render('front/register.html.twig',['form'=>$form->createView()]);
     }
 
     /**
@@ -125,5 +152,17 @@ class FrontController extends AbstractController
         return $this->render('front/_main_categories.html.twig',[
             'categories'=>$categories
         ]);
+    }
+
+    private function loginUserAutomatically($user, $password)
+    {
+        $token = new UsernamePasswordToken(
+            $user,
+            $password,
+            'main', // security.yaml
+            $user->getRoles()
+        );
+        $this->get('security.token_storage')->setToken($token);
+        $this->get('session')->set('_security_main',serialize($token));
     }
 }
